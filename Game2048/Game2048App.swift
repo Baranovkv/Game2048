092d7dd4eaa002b08db919d0026bea92eb258347
Kirill <K-baranov@outlook.com>
2024-04-07 18:48:15 +0200

    Initial Commit

---
+++ Game2048/Game2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Game2048App.swift
+//  Game2048
+//
+//  Created by Kirill Baranov on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Game2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
